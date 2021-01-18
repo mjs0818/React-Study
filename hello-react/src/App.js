8d753706455f01295a438740e2649e2029432552
mjs0818 <tjfaudwo09@gmail.com>
2021-01-18 20:25:20 +0900

    ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Counter from './Counter';
 import EventPractice from './EventPractice';
 import MyComponent from './MyComponent';
 import Say from './Say';
-const App = () => {
-  return <EventPractice />;
-};
+import ValidationSample from './ref/ValidationSample';
+import ScrollBox from './ref/ScrollBox';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
