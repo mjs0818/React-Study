812fce908a7bb8a14e85a983452039a736560919
mjs0818 <tjfaudwo09@gmail.com>
2021-02-18 21:59:30 +0900

    react hooks

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,11 +1,12 @@
 import { Component, useState } from 'react';
+import Average from './Average';
 import CounterUseReducer from './CounterUseReducer';
 import Info from './Info';
 
 function App() {
   const [visible, setVisible] = useState(false);
 
-  return <Info></Info>;
+  return <Average />;
 }
 
 export default App;
