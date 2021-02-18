9f8c94c1edd61bcf84489a032ee7816353267f91
mjs0818 <tjfaudwo09@gmail.com>
2021-02-18 22:48:25 +0900

    Commit before yarn eject

---
+++ styling-react/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import SassCopmonent from './SassComponent';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassCopmonent />
+      </div>
+    );
+  }
+}
