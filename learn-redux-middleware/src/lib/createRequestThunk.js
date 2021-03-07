01824c572ad5ddab2022452391c08b5a8321fb8c
mjs0818 <tjfaudwo09@gmail.com>
2021-03-07 21:57:07 +0900

    redux-thunk

---
+++ learn-redux-middleware/src/lib/createRequestThunk.js
@@ -0,0 +1,23 @@
+export default function createRequestThunk(type, request) {
+  // 성공 및 실패 액션 타입을 정의
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return (params) => async (dispatch) => {
+    dispatch({ type }); // 시작
+    try {
+      console.log(params);
+      const response = await request(params);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      }); // 성공
+    } catch (e) {
+      dispatch({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+      throw e;
+    }
+  };
+}
