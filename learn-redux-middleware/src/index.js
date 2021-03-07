9a0583d3ababbf8a04e32c296fdef5dffc88741a
mjs0818 <tjfaudwo09@gmail.com>
2021-03-07 19:55:44 +0900

    learn-redux-middleware

---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules/index';
+import loggerMiddleware from './lib/loggerMiddleware';
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
