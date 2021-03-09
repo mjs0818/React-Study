7df231e3dd4f0940bd8e998c9b6ed2d98d1c7069
mjs0818 <tjfaudwo09@gmail.com>
2021-03-09 21:12:51 +0900

    redux-saga counter

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -13,12 +13,14 @@ const SampleContainer = ({
   loadingUsers,
 }) => {
   useEffect(() => {
+    // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+    // 그 내부에서 async 함수를 선언하고 호출해 준다.
     const fn = async () => {
       try {
         await getPost(1);
         await getUsers(1);
       } catch (e) {
-        console.log(e); // 에러 조ㅗ히
+        console.log(e); // 에러 조회
       }
     };
     fn();
